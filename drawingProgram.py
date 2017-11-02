41eecb6a3efa7f16f828fe9d1c2e914fd719fafc
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-11-02 14:07:43 -0400

    Updated from Brython Server: 11/2/2017 2:07:43 PM

--- drawingProgram.py
+++ drawingProgram.py
@@ -2,3 +2,16 @@
 #
 #drawingProgram
 
+from ggame import *
+
+white = Color(0xFFFFFF,1)
+black = Color(0x000000,1)
+red = Color(0xFF0000,1) 
+yellow = Color(0xFFFF00,1)
+orange = Color(0xFFA500,1)
+green = Color(0x00FF00,1) 
+lightBlue = Color(0x00FFFF,1) 
+blue = Color(0x0000FF,1) 
+purple = Color(0x7F00FF,1) 
+darkPink = Color(0xFF007F,1) 
+
