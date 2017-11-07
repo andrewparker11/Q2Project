2552a95ff97d5346c6cab0c8c2dee87f75e6eeb0
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-11-07 14:35:21 -0500

    Updated from Brython Server: 11/7/2017 2:35:20 PM

--- drawingProgram.py
+++ drawingProgram.py
@@ -21,19 +21,22 @@ def moveRight(event):
 def moveLeft(event):
     if painter.x > 0:
         painter.x -= CELL_SIZE
-        Sprite(startBox,(painter.x,painter.y))
+        if data["drawOnOff"] == True:
+            Sprite(startBox,(painter.x,painter.y))
     
 #moves painter up    
 def moveUp(event):
     if painter.y > 0:
         painter.y -= CELL_SIZE
-        Sprite(startBox,(painter.x,painter.y))
+        if data["drawOnOff"] == True:
+            Sprite(startBox,(painter.x,painter.y))
     
 #moves painter down
 def moveDown(event):
     if painter.y < (ROWS-1)*CELL_SIZE:
         painter.y += CELL_SIZE
-        Sprite(startBox,(painter.x,painter.y))
+        if data["drawOnOff"] == True:
+            Sprite(startBox,(painter.x,painter.y))
 
 
 #drawOff
